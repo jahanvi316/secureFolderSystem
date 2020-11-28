dc7874a52c691a758300e2f82b81c9bb00f6cc98
Jahanvi Patel <jahanvi316@gmail.com>
2020-11-28 02:41:06 -0500

    v8
    What is left:
    - Main Page: ADD LOGO
    - EditFile : DELETE FILE
    - ViewFile: VIEW FILE
    
    OPTIONAL (IF TIME PERMITS):
    Edit the way file names come on both lists

--- secureFolderSystem/app/src/main/java/com/example/securefoldersystem/ViewOnly.java
+++ secureFolderSystem/app/src/main/java/com/example/securefoldersystem/ViewOnly.java
@@ -8,8 +8,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -30,23 +34,51 @@ public class ViewOnly extends AppCompatActivity {
                 startActivity(logout);
             }
         });
+        final ListView viewFiles = (ListView) findViewById(R.id.files_view);
+
         context = getApplicationContext();
         ArrayList<File> fileList = new ArrayList<>();
-        File filesDir = context.getDir("files", context.MODE_PRIVATE);
-        File[] allFiles = filesDir.listFiles();
-        if (allFiles != null && allFiles.length > 0) {
-            for (File f : allFiles)  {
-                if (f.isFile()) {
-                    fileList.add(f);
-                    System.out.println("File added: " + f);
-                }
-            }
+
+        String[] allFiles = context.fileList();
+        for (String f : allFiles)  {
+            Log.e("files for loop", "f: " + f);
+            File fileName = context.getDir(f, Context.MODE_PRIVATE);
+            fileList.add(fileName);
         }
 
-        RecyclerView viewFiles = (RecyclerView) findViewById(R.id.filesView);
-        ListAdapter fAdapter = new ListAdapter(this.getLayoutInflater(), fileList);
-        viewFiles.setLayoutManager(new LinearLayoutManager(this));
-        viewFiles.setAdapter(fAdapter);
+        ArrayAdapter<File> fileArrayAdapter;
+        fileArrayAdapter = new ArrayAdapter<File>(this, android.R.layout.simple_list_item_1, fileList);
+        viewFiles.setAdapter(fileArrayAdapter);
+        viewFiles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // System.out.println("EDIT FILE SET ON ITEM CLICK LISTENER PRESSED");
+                File clickedItem = (File) viewFiles.getItemAtPosition(position);
+                Intent viewFile = new Intent(ViewOnly.this, ViewFile.class);
+                String fileName = clickedItem.getName();
+                viewFile.putExtra("fileSelected", fileName);
+                startActivity(viewFile);
+            }
+
+        });
+
+//        ArrayList<File> fileList = new ArrayList<>();
+//        File filesDir = context.getDir("files", context.MODE_PRIVATE);
+//        File[] allFiles = filesDir.listFiles();
+//        if (allFiles != null && allFiles.length > 0) {
+//            for (File f : allFiles)  {
+//                if (f.isFile()) {
+//                    fileList.add(f);
+//                    System.out.println("File added: " + f);
+//                }
+//            }
+//        }
+//
+//        RecyclerView viewFiles = (RecyclerView) findViewById(R.id.filesView);
+//        ListAdapter fAdapter = new ListAdapter(this.getLayoutInflater(), fileList);
+//        viewFiles.setLayoutManager(new LinearLayoutManager(this));
+//        viewFiles.setAdapter(fAdapter);
+
     }
 
 }
